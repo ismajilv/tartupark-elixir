7ad777bc8377025885b704f2d5384cf33c64b96b
soqara <soqara@ttu.ee>
2017-11-29 21:26:04 +0200

    maps is loading..

--- config/config.exs
+++ config/config.exs
@@ -32,3 +32,5 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :tartupark, gmaps_api_key: "AIzaSyCDEm-ZYWAGGfLsX54mAjVV-GbyRI-_CDU"
