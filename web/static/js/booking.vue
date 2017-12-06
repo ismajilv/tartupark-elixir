5b7f1c04342c4c535010884972fadf73adb0a3bd
soqara <soqara@ttu.ee>
2017-12-06 23:02:05 +0200

    errors are fixed..

--- web/static/js/booking.vue
+++ web/static/js/booking.vue
@@ -139,12 +139,13 @@ export default {
                 var paymentTime = this.h_payment_selected;
               }
 
-              console.log(parkingStartTime.toISOString());
+              var startTime = (parkingStartTime != null ) ? parkingStartTime.toISOString() : null;
+              var endTime = (parkingEndTime != null) ? parkingEndTime.toISOString() : null;
 
               axios.post("/api/search",
                   { lngLat: lngLat,
-                    parkingStartTime: parkingStartTime.toISOString(),
-                    parkingEndTime: parkingEndTime.toISOString(),
+                    parkingStartTime: startTime,
+                    parkingEndTime: endTime,
                     parkingSearchRadius: this.parking_search_radius,
                     paymentTime: paymentTime,
                     paymentType: this.payment_type
