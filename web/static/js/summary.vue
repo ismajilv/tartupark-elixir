be684adb5ba44adb5cb8058b58868f6c0bff2d5b
soqara <soqara@ttu.ee>
2017-12-13 16:04:50 +0200

    summar.vue end parking works. cancel booking is checking...

--- web/static/js/summary.vue
+++ web/static/js/summary.vue
@@ -21,12 +21,38 @@
                     <td>{{ (booking.payment != null) ? booking.payment.payment_code : null }}</td>
                     <td>{{ (booking.payment != null) ? (booking.payment.cost).toFixed(2) : null }}</td>
                     <td class="text-right">
-                        <button type="submit" v-if="booking.paymentType == 'Real Time'" class="btn btn-info btn-xs" id="btn_end" v-on:click="endParking(booking.booking_id)">End Parking</button>
+                        <!-- <button type="submit" v-if="booking.paymentType == 'Real Time'" class="btn btn-info btn-xs" id="btn_end" v-on:click="endParking(booking.booking_id)">End Parking</button> -->
+                        <button v-if="booking.paymentType == 'Real Time'" class="btn btn-info btn-xs" @click="showModal=true" v-on:click="sendInfoToPopUp(booking.booking_id)" id="btn_submit">End Parking</button>
                         <button type="submit" v-if="booking.paymentType == 'Hourly'" class="btn btn-danger btn-xs" id="btn_cancel" v-on:click="cancelBooking(booking.booking_id)">Cancel Booking</button>
                     </td>
                 </tr>
             </tbody>
         </table>
+
+        <my-modal v-show="showModal" @close="showModal=false">
+            <form class="col-md-10 col-md-offset-1" style="padding:0">
+                <div class="form-group col-md-12">
+                    <input type="text" class="form-control" id="cardNumber" v-model="cardNumber" placeholder="Card Number" contenteditable="false">
+                </div>
+                <p style="text-align:center;">Expiration</p>
+                <div class="form-group col-md-6">
+                    <input type="text" class="form-control" id="cardMonth" v-model="cardMonth" placeholder="Month" contenteditable="false">
+                </div>
+                <div class="form-group col-md-6">
+                    <input type="text" class="form-control" id="cardYear" v-model="cardYear" placeholder="Year" contenteditable="false">
+                </div>
+                <div class="form-group col-md-12">
+                    <input type="text" class="form-control" id="cardPAC" v-model="cardPAC" placeholder="Premier Access Code">
+                </div>
+                <div class="clearfix"></div>
+            </form>
+
+            <div class="row text-center">
+                <hr>
+                <button type="submit" class="btn btn-primary col-md-8 col-md-offset-2" id="btn_pay" v-on:click="endParking()">Pay</button>
+                <br class="">
+            </div>
+        </my-modal>
     </div>
 </template>
 
@@ -40,13 +66,18 @@ export default {
     data: function() {
         return {
             bookings: [],
-            selectedBookingForUpdate: null
+            selectedBookingForUpdate: null,
+            bookingId: null,
+            showModal: false,
+            cardNumber: "19293748203910",
+            cardMonth: "12",
+            cardYear: "20",
+            cardPAC: "123"
         }
     },
     methods: {
-        endParking: function(bookingId, parkingEndTime){
-            console.log("booking id: " + bookingId);
-            axios.patch("/api/bookings/"+bookingId,
+        endParking: function(){
+            axios.patch("/api/bookings/"+ this.bookingId,
             {parkingEndTime: new Date()}, 
             {headers: auth.getAuthHeader()})
             .then(response => {
@@ -58,8 +89,7 @@ export default {
         },
         cancelBooking: function(bookingId){
             console.log("booking id: " + bookingId);
-            axios.delete("/api/bookings/"+bookingId,
-            {bookin_id: bookingId}, 
+            axios.delete("/api/bookings/"+bookingId, 
             {headers: auth.getAuthHeader()})
             .then(response => { 
                 console.log(response.data);
@@ -67,6 +97,9 @@ export default {
             .catch(error => {
                 console.log(error);
             });
+        },
+        sendInfoToPopUp: function(bookingId){
+            this.bookingId = bookingId;
         },
         getSummary: function(){
             axios.get("/api/bookings/summary", {headers: auth.getAuthHeader()})
