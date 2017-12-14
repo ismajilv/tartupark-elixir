e5c988f651a31bc0895ce1fcc5a850d144c583ca
Polad Mahmudov <pomahm@gitlab.cs.ttu.ee>
2017-12-14 15:15:20 +0200

    booking and payment back-end altered

--- web/controllers/booking_api_controller.ex
+++ web/controllers/booking_api_controller.ex
@@ -23,13 +23,13 @@ defmodule Tartupark.BookingAPIController do
                     inserted_at: book.inserted_at,
                     paymentTime: book.paymentTime,
                     paymentType: book.paymentType,
-                    cancelationPermission: compareCurrentAndEndTime(NaiveDateTime.utc_now(), book.endDateTime),
+                    cancelationPermission: compareCurrentAndEndTime(NaiveDateTime.utc_now(), book.startDateTime) == :gt,
                     status: book.status,
                     place: Enum.map(book.place.area.coordinates,
                                      fn point -> {lng, lat} = point
                                         %{lng: lng, lat: lat}
                                      end),
-                    # cost:  Tartupark.PaymentAPIController.payment_calculate(book, book.place.zone),
+                    cost:  Tartupark.PaymentAPIController.payment_calculate(book, book.place.zone),
                     payment: case book.payment do
                               nil -> nil
                               payment ->  %{payment_code: payment.payment_code,
@@ -38,7 +38,7 @@ defmodule Tartupark.BookingAPIController do
                                             payment_id: payment.id}
                              end}
                 end)
-      |> Enum.filter(fn book -> book.status == "active" end)
+      |> Enum.filter(fn book -> book.status == "available" end)
 
       conn
       |> put_status(201)
@@ -62,7 +62,7 @@ defmodule Tartupark.BookingAPIController do
          "Hourly" -> end_time = parseToNaiveDateTime(endDateTime)
           _       -> end_time = nil
      end
-     booking_params = %{startDateTime: start_time, endDateTime: end_time, paymentTime: paymentTime, paymentType: paymentType, status: "active"}
+     booking_params = %{startDateTime: start_time, endDateTime: end_time, paymentTime: paymentTime, paymentType: paymentType, status: "available"}
      booking_place_bind = Ecto.build_assoc(Repo.get(Place, place_id), :bookings, booking_params)
      booking = Ecto.build_assoc(user, :bookings, booking_place_bind)
                |> Repo.insert!
@@ -100,14 +100,20 @@ defmodule Tartupark.BookingAPIController do
 
   def delete(conn, %{"booking_id" => booking_id}) do
     booking = Repo.get!(Booking, booking_id)
-    changeset = Ecto.Changeset.change(booking, status: "deactive")
-    case Repo.update changeset do
-      {:ok, _struct}       -> conn
-                               |> put_status(200)
-                               |> json(%{msg: "Booking 'id: #{booking.id}' was deleted successfully."})
-      {:error, _changeset} -> conn
-                               |> put_status(400)
-                               |> json(%{msg: "Booking 'id: #{booking.id}' could not be deleted."})
+    if NaiveDateTime.compare(booking.startDateTime, NaiveDateTime.utc_now()) == :lt do
+      changeset = Ecto.Changeset.change(booking, status: "canceled")
+      case Repo.update changeset do
+        {:ok, _struct}       -> conn
+                                 |> put_status(200)
+                                 |> json(%{msg: "Booking 'id: #{booking.id}' was deleted successfully."})
+        {:error, _changeset} -> conn
+                                 |> put_status(400)
+                                 |> json(%{msg: "Booking 'id: #{booking.id}' could not be deleted."})
+      end
+    else
+      conn
+       |> put_status(423)
+       |> json(%{msg: "Booking 'id: #{booking.id}' could not be deleted. Too late."})
     end
   end
 
@@ -143,7 +149,7 @@ defmodule Tartupark.BookingAPIController do
                             capacity: List.foldl(park_place.bookings,
                                                  park_place.capacity,
                                                  fn (book, capacity) ->
-                                                   if book.status == "active" do
+                                                   if book.status == "available" do
                                                      case checkBetweenOrAfter(start_time, end_time, book.startDateTime, book.endDateTime) do
                                                        true -> capacity-1
                                                        false -> capacity
@@ -156,9 +162,9 @@ defmodule Tartupark.BookingAPIController do
                             endDateTime: end_time,
                             parkingSearchRadius: params["parkingSearchRadius"],
                             paymentTime: params["paymentTime"],
-                            paymentType: params["paymentType"]
-                            # cost: Tartupark.PaymentAPIController.payment_calculate(%{startDateTime: start_time, endDateTime: end_time, paymentType: params["paymentType"]},
-                            #                                                        %{costHourly: park_place.zone.costHourly, costRealTime: park_place.zone.costRealTime})
+                            paymentType: params["paymentType"],
+                            cost: Tartupark.PaymentAPIController.payment_calculate(%{startDateTime: start_time, endDateTime: end_time, paymentType: params["paymentType"]},
+                                                                                   %{costHourly: park_place.zone.costHourly, costRealTime: park_place.zone.costRealTime})
                           }
                           end))
 
@@ -171,13 +177,13 @@ defmodule Tartupark.BookingAPIController do
   end
 
   def parseToNaiveDateTime(dateTime) when dateTime != nil do
-    scannedDateTime = Regex.scan(~r/\d+/, dateTime, trim: true)
+    startDateTime = Regex.scan(~r/\d+/, dateTime, trim: true)
     |> List.flatten
     |> Enum.map(fn calendarElem -> Integer.parse(calendarElem) |> elem(0)  end)
-    case length(scannedDateTime) do
-       6 -> [year, month, day, hour, minute, second] = scannedDateTime
-       7 -> [year, month, day, hour, minute, second, _millisecond] = scannedDateTime
-       8 -> [year, month, day, hour, minute, second, _timezone1, _timezone2] = scannedDateTime
+    case length(startDateTime) do
+       6 -> [year, month, day, hour, minute, second] = startDateTime
+       7 -> [year, month, day, hour, minute, second, _millisecond] = startDateTime
+       8 -> [year, month, day, hour, minute, second, _timezone1, _timezone2] = startDateTime
     end
     NaiveDateTime.new(year, month, day, hour, minute, second) |> elem(1)
   end
