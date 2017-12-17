62c083649bb8c508cd3b47c457d5277195da5074
Tural Ismayilov <tuisma@ttu.ee>
2017-12-17 17:14:03 +0200

    White bread scenario and context updated

--- features/contexts/white_bread_context.exs
+++ features/contexts/white_bread_context.exs
@@ -19,107 +19,43 @@ defmodule WhiteBreadContext do
     Hound.end_session
   end
 
-  given_ ~r/^the following person, user register on the STRS Customer app$/, fn state,
+  given_ ~r/^the following user$/, fn state,
   %{table_data: table} ->
-    [%{fullName: "ns", username: "ns", password: "ns", email: "customer@gmail.com", license_number: "123"}]
-    # table
+     table
      |> Enum.map(fn user_data -> User.changeset(%User{}, user_data) end)
      |> Enum.each(fn changeset -> Repo.insert!(changeset) end)
-     |> IO.inspect
-    # {:ok, state}
+     {:ok, state}
   end
 
-  when_ ~r/^I enter the booking information on the STRS Customer app$/, fn state ->
-    session = to_string(Enum.random(0..100000))
+  when_ ~r/^User log in on the app$/, fn state ->
+    in_browser_session String.to_atom(to_string(1)), fn ->
 
-    in_browser_session String.to_atom(session), fn ->
-      navigate_to "/#/login"
-
+      navigate_to "/"
       fill_field({:id, "log-username"}, "ns")
-      fill_field({:id, "log-password"}, "ns")
-
+      fill_field({:id, "log-password"}, "parool")
       click({:id, "login-for-test"})
 
-      Process.sleep(30000)
+      fill_field({:id, "log-username"}, "ns")
+      fill_field({:id, "log-password"}, "parool")
+      click({:id, "login-for-test"})
 
+      Process.sleep(1000)
     end
     {:ok, state}
-  end
+    end
 
-  then_ ~r/^I press the registration button$/, fn state ->
-    {:ok, state}
-  end
+    then_ ~r/^User enter the parking address of "(?<destinations>[^"]+)", "(?<payment_type>[^"]+)", "(?<start_date>[^"]+)", "(?<end_date>[^"]+)" and "(?<search_radius>[^"]+)"$/,
+    fn state, %{destinations: destinations,payment_type: payment_type,start_date: start_date,end_date: end_date,search_radius: search_radius} ->
+      in_browser_session String.to_atom(to_string(1)), fn ->
+        click({:id, "btn_map"})
+        fill_field({:id, "parking_address"}, to_string(destinations) <> ", Tartu, Estonia")
+        Process.sleep(10000)
+        #fill_field({:id, parking_start_time}, "")
+      end
+      {:ok, state}
+    end
 
-  and_ ~r/^I fill the registration page with my details and submit$/, fn state ->
-    {:ok, state}
-  end
-
-  and_ ~r/^The web page is directed to Main page$/, fn state ->
-    {:ok, state}
-  end
-
-  when_ ~r/^I enter the parlking address of "(?<argument_one>[^"]+)"$/,
-  fn state, %{argument_one: _argument_one} ->
-    {:ok, state}
-  end
-
-  then_ ~r/^I specify payment type of ""$/, fn state ->
-    {:ok, state}
-  end
-
-  and_ ~r/^I specify start date of ""$/, fn state ->
-    {:ok, state}
-  end
-
-  and_ ~r/^I specify start date of "(?<argument_one>[^"]+)"$/,
-  fn state, %{argument_one: _argument_one} ->
-    {:ok, state}
-  end
-
-  and_ ~r/^I spesify search radius of "(?<argument_one>[^"]+)"$/,
-  fn state, %{argument_one: _argument_one} ->
-    {:ok, state}
-  end
-
-  and_ ~r/^I want to park my car in "(?<argument_one>[^"]+)"$/,
-  fn state, %{argument_one: _argument_one} ->
-    {:ok, state}
-  end
-
-  and_ ~r/^I press choose$/, fn state ->
-    {:ok, state}
-  end
-
-  and_ ~r/^I get notification of "(?<argument_one>[^"]+)"$/,
-  fn state, %{argument_one: _argument_one} ->
-    {:ok, state}
-  end
-
-  when_ ~r/^Payment method is Hourly and payment time is Before Parking$/, fn state ->
-    {:ok, state}
-  end
-
-  then_ ~r/^I add credit card details$/, fn state ->
-    {:ok, state}
-  end
-
-  and_ ~r/^I submit payment$/, fn state ->
-    {:ok, state}
-  end
-
-  when_ ~r/^Payment method is Hourly and payment time is End of month$/, fn state ->
-    {:ok, state}
-  end
-
-  then_ ~r/^I redirected to main page$/, fn state ->
-    {:ok, state}
-  end
-
-  when_ ~r/^Payment method is Real time and payment time is Before Parking$/, fn state ->
-    {:ok, state}
-  end
-
-  then_ ~r/^I directed to main page$/, fn state ->
-    {:ok, state}
-  end
+    and_ ~r/^User press choose$/, fn state ->
+      {:ok, state}
+    end
 end
--- features/tartupark.feature
+++ features/tartupark.feature
@@ -8,33 +8,10 @@ Feature: Parking allocation
           | fullName      | username | password | email     | license_number |
           | Name          | username | parool   | ns@ns.com | ns123          |
     When User log in on the app
-    Then User enter the parking address of "<destinations>"
-    And User specify payment type of "<payment_type>"
-    And User specify start date of "<start_date>"
-    And User specify start date of "<end_date>"
-    And User spesify search radius of "<search_radius>"
-    And User want to park his car in "<street choice>"
+    Then User enter the parking address of "<destinations>", "<payment_type>", "<start_date>", "<end_date>" and "<search_radius>"
     And User press choose
-    And User enter the parking address of "<destinations>"
-    And User specify payment type of "<payment_type>"
-    And User specify start date of "<start_date>"
-    And User specify start date of "<end_date>"
-    And User spesify search radius of "<search_radius>"
-    And User want to park his car in "<street choice>"
-    And User press choose
-    And User add payment details and pay
-    And User enter the parking address of "<destinations>"
-    And User specify payment type of "<payment_type>"
-    And User specify start date of "<start_date>"
-    And User specify start date of "<end_date>"
-    And User spesify search radius of "<search_radius>"
-    And User want to park his car in "<street choice>"
-    And User press choose
-    When User enter to booking history
-    Then User see all booked places
-    And User pay the booking place with end of month
-    And User cancel all of them and return to main page
+
 
     Examples:
-        | destinations     | real_time_configured    | start_date | payment_type | street_choice  | payment_choice  | start_time_choice | end_time_choice | notification |
-        | Tamme puiestee 1 | yes (balance 12.5 euro) | | |Vanemuise      | hourly          | 15:00             | 15:20           | parking space for 20 mins is booked and your fee is 2 euros |
+        | destinations     | payment_type    | start_date | end_date | search_radius  |
+        | Tamme puiestee 1 | Hourly          | Vanemuise  | 4:20     | 500            |
